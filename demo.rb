b9d3837ffef411b5da603e86c5724cad2360eddc
ykumawat <ykumawat@ykumawat-desktop.(none)>
2012-02-16 17:59:51 +0530

    till array1 & array2 method of array

--- demo.rb
+++ demo.rb
@@ -1,1 +1,34 @@
-puts "Hello Ruby"
+#This is Array program program
+
+names=Array.new()
+names[0]=12;
+names[1]=13;
+puts"value of first array is #{names[0]}"
+puts"value of second array is #{names[1]}"
+puts"value of array using loop"
+#check the size of array
+puts "size of array before add new element is #{names.length}"
+ names[2]=23;
+ 
+for i in 0..1
+ puts "value of #{i} array is #{names[i]}"
+ end
+#check the size of array
+puts "size of array is #{names.length}"
+
+# array & other array
+first=Array[3,5,40,7,9,11,3,4]   #create first array
+second=Array[3,4,5,6,7,8,9,10,4] #create second array
+third=Array.new #create third array
+
+third=first & second
+for i in 0...third.length 
+puts"Third array #{i} value #{third[i]}"
+end
+
+
+
+
+
+
+
