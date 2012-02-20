a18d2d4959d97cfb349b34104be065738e37065f
ykumawat <ykumawat@ykumawat-desktop.(none)>
2012-02-20 15:04:00 +0530

    first() commands

--- array.rb
+++ array.rb
@@ -1,18 +1,11 @@
-#fill: 
+#first: 
 class ArrayPro
 
  def arrayFun
  f=Array[5,7,8,6,29,16]
  s=Array["a","b","c","d","c"]
- print("#{s}")
- printf('first')
- print s.fill("1")
- print('second')
- puts s.fill("#",2,2)
- puts 'third'
- puts s.fill("&",0..2)
- puts'fourth'
-
+ puts "first element #{f.first}"
+puts "first element #{f.first(17)}"
 
   
    
