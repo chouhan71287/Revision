12efccabff7caa15b77ba2815f52c878f2c7bc65
ykumawat <ykumawat@ykumawat-desktop.(none)>
2012-02-20 14:25:57 +0530

    fetch

--- array.rb
+++ array.rb
@@ -1,12 +1,15 @@
-#eql?: 
+#fetch: 
 class ArrayPro
 
  def arrayFun
- first=Array[5,7,8,6,9,6]
+ first=Array[5,7,8,6,29,16]
  second=Array["a","b","c","d","c"]
-  
- puts first.eql?(first)
- puts [].empty?
+ print first
+ puts first.fetch(1)
+ puts first.fetch(5)
+ puts first.fetch(5,"vijay")
+ puts first.fetch(4){|x| x*x}
+
   
    
 
