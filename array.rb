9e208bc0836f9006dc4a14d704205a28efb6c1e6
ykumawat <ykumawat@ykumawat-desktop.(none)>
2012-02-20 11:51:35 +0530

    drop_while

--- array.rb
+++ array.rb
@@ -1,10 +1,10 @@
-#delete_if: 
+#drop_while: 
 class ArrayPro
 
  def arrayFun
  first=Array[5,7,8,6,9,6]
  second=Array["a","b","c","d","c"]
- puts first.delete_if{|x| x<8}
+ puts first.drop_while{|x| x<6}
  puts "#{first}"
   
    
