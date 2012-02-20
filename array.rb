51bead29417e657ce68165ae9af2a435b0d4ae9d
ykumawat <ykumawat@ykumawat-desktop.(none)>
2012-02-20 11:45:24 +0530

    delete_if

--- array.rb
+++ array.rb
@@ -1,11 +1,11 @@
-#delete_at(index): 
+#delete_if: 
 class ArrayPro
 
  def arrayFun
  first=Array[5,7,8,6,9,6]
  second=Array["a","b","c","d","c"]
- puts "#{second.delete_at(2)}"
- puts "#{second}"
+ puts first.delete_if{|x| x<8}
+ puts "#{first}"
   
    
 
