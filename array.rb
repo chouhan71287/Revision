a5d259d4cfafe82927344eed4dd0068877e69479
chouhan71287 <vchouhan@systematixtechnocrates.com>
2012-02-20 16:54:07 +0530

    pop

--- array.rb
+++ array.rb
@@ -1,10 +1,13 @@
-#first: 
+#pop: 
 class ArrayPro
 
  def arrayFun
  f=Array[3,4,5,6,7]
- s=Array["a","b","c","d","e"]
- puts s.replace(["l","m"])
+ s=Array["a","b","c","d","e","f","g","i","j","k"]
+ print s;
+ #print ("\n #{s.pop}")
+ #print ("\n #{s.pop(2)}")
+ print ("\n #{s.pop(3)} \n")
  
 
 
