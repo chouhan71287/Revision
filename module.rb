d68879fa0e139cfe849ff657bd54e791c83e0bc1
chouhan71287 <vchouhan@systematixtechnocrates.com>
2012-02-21 17:33:05 +0530

    module with class

--- module.rb
+++ module.rb
@@ -1,19 +1,14 @@
-require 'trig'
-require 'moral'
-require 'test.rb'
+require 'classmodule'
 class A
-include Test
-include Trig
 
+ def employee(name="SYSTANGO")
+ @name_of_employee=name
+ end
 
-def initialize()
-y=Trig.div(20)
-puts "value is #{y}"
-z=Moral.city("Indore")
-puts "city = #{z}"
-puts "#{Trig::SUM}"
-
-end
 end
 ob1=A.new
-puts ob1.hello
+puts ob1.employee
+ob2=ClassModule::Student.new
+puts ob2.result(12,33,44,5,22)
+ob3=ClassModule::Student.new
+puts ob3.result(44,55,66,77)
