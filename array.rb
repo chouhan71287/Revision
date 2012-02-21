af493da4d8d3746a6862fba70e9cd6c3cff15ae4
chouhan71287 <vchouhan@systematixtechnocrates.com>
2012-02-21 12:29:53 +0530

    modified array

--- array.rb
+++ array.rb
@@ -3,11 +3,10 @@ class ArrayPro
 
  def arrayFun
  f=Array[3,4,5,6,7]
- s=Array["a","b","c","d","e","f","g","i","j","k"]
+ s=Array["a","b","b","c","d","e","c","f","g","i","j","k"]
  print s;
- #print ("\n #{s.pop}")
- #print ("\n #{s.pop(2)}")
- print ("\n #{s.pop(3)} \n")
+ print("\n \n")
+ print(s.rindex("c"))
  
 
 
