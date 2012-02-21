8aa8d37e61a9db3598440f9d0f48c02d679826b8
chouhan71287 <vchouhan@systematixtechnocrates.com>
2012-02-21 13:26:58 +0530

    hash->integar

--- method.rb
+++ method.rb
@@ -1,33 +1,13 @@
 class Method_Test
 #@total=0
-def test_value(var1="default_value", var2="Default_value 2", var3="Default_value3")  
-  puts "value of variables are #{var1}, #{var2} and #{var3}"
-end
-  
- def initialize()
-        @total = 0
-    end 
-def result(*test)
-puts "=================RESULT=================================="
- puts "Number of Subjects are #{test.length}"
- puts "Calculating result"
- puts test.inspect
- puts test.to_s # with to_s method
- a=test.inspect #  with inspect method
- puts a
- for i in 0...test.length
-@total=@total + test[i]
-end
+ def abc(var1,var2)
+ end
 
-return @total 
-   
-end
 end
 
 ob1=Method_Test.new
-#ob1.test_value("vijay",200)
-
-#ob1.test_value("SCS",25000)
-marks=ob1.result(1,2,2)
- puts "Your Total marks is #{marks}"
+ob2=Method_Test.new
+ob1.abc 10,20
+ob2.abc 10,20
+puts ob1.hash
 
